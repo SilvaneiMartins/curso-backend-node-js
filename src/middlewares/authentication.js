447f18bf5984341fb6f083d7105ee321db0761d2
Silvanei Martins <silvaneimartins_rcc@hotmail.com>
2023-03-10 11:03:54 -0300

    Metodo Controller get me

--- src/middlewares/authentication.js
+++ src/middlewares/authentication.js
@@ -1,8 +1,10 @@
-import { jwt } from '../utils/index.js';
+import { jwt } from "../utils/index.js";
 
 function asureAuth(req, res, next) {
-    if (!res.headers.authorization) {
-        return res.status(403).send({ message: 'Você não tem autorização para acessar a aplicação!' });
+    if (!req.headers.authorization) {
+        return res
+            .status(403)
+            .send({ msg: "Você não tem autorização para acessar o sistema!" });
     }
 
     const token = req.headers.authorization.replace("Bearer ", "");
@@ -11,18 +13,18 @@ function asureAuth(req, res, next) {
         const hasExpired = jwt.hasExpiredToken(token);
 
         if (hasExpired) {
-            return res.status(400).send({ message: 'Token expirado!' });
+            return res.status(400).send({ msg: "O token está expirado!" });
         }
 
         const payload = jwt.decoded(token);
-        req.user = payload
+        req.user = payload;
 
         next();
     } catch (error) {
-        console.log(error);
+        res.status(400).send({ msg: "Token inválido!" });
     }
 }
 
 export const mdAuth = {
     asureAuth,
-}
+};
--- src/routes/user.js
+++ src/routes/user.js
@@ -1,10 +1,10 @@
 import express from "express";
 
-import { mdAuth } from '../middlewares/index.js';
-import { UserController } from '../controllers/index.js';
+import { mdAuth } from "../middlewares/index.js";
+import { UserController } from "../controllers/index.js";
 
 const api = express.Router();
 
-api.get('/user/me', UserController.getMe);
+api.get("/user/me", [mdAuth.asureAuth], UserController.getMe);
 
 export const userRoutes = api;
