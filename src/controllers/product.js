cc8b1a1059d8bda6b64b38739e262076a9dd6f20
Silvanei Martins <silvaneimartins_rcc@hotmail.com>
2023-04-04 10:49:29 -0300

    Finalizado do metodo update product

--- src/controllers/product.js
+++ src/controllers/product.js
@@ -1,4 +1,5 @@
-import { Product } from '../models/index.js';
+import { Product } from "../models/index.js";
+import { getFilePath } from "../utils/index.js";
 
 function createProduct(req, res) {
     const { name, description, category, price, quantity } = req.body;
@@ -12,28 +13,30 @@ function createProduct(req, res) {
     });
 
     product.save((error, productStored) => {
-        if (error)  {
-            return res.status(500).send({ msg: 'Error de servidor!' });
+        if (error) {
+            return res.status(500).send({ msg: "Error de servidor!" });
         } else {
             return res.status(201).send(productStored);
         }
     });
-};
+}
 
 async function getAllProducts(req, res) {
     try {
         const { _id } = req.body;
-        const products = await Product.find({ _id: { $ne: _id } }).select(["-__v"]);
+        const products = await Product.find({ _id: { $ne: _id } }).select([
+            "-__v",
+        ]);
 
         if (!products) {
-            return res.status(400).send({ msg: 'Produto não encontrado!' });
+            return res.status(400).send({ msg: "Produto não encontrado!" });
         } else {
             return res.status(200).send(products);
         }
     } catch (error) {
-        return res.status(500).send({ msg: 'Error de servidor!' });
+        return res.status(500).send({ msg: "Error de servidor!" });
     }
-};
+}
 
 async function getProductById(req, res) {
     const { id } = req.params;
@@ -42,22 +45,65 @@ async function getProductById(req, res) {
         const response = await Product.findById(id).select(["-__v"]);
 
         if (!response) {
-            res.status(400).send({ msg: 'Produto não encontrado!' });
+            res.status(400).send({ msg: "Produto não encontrado!" });
         } else {
             res.status(200).send(response);
         }
     } catch (error) {
-        res.status(500).send({ msg: 'Error de servidor!' });
+        res.status(500).send({ msg: "Error de servidor!" });
     }
-};
+}
 
 async function updateProduct(req, res) {
-    return res.status(200).send({ msg: 'Atualizar produto!' });
-};
+    const { id } = req.params;
+    const { name, quantity, price, category, description } = req.body;
+
+    const product = await Product.findById(id);
+
+    if (name) {
+        product.name = name;
+    }
+
+    if (quantity) {
+        product.quantity = quantity;
+    }
+
+    if (price) {
+        product.price = price;
+    }
+
+    if (category) {
+        product.category = category;
+    }
+
+    if (description) {
+        product.description = description;
+    }
+
+    if (req.files.image) {
+        const imagePath = getFilePath(req.files.image);
+        product.image = imagePath;
+    }
+
+    Product.findByIdAndUpdate(id, product, (error) => {
+        if (error) {
+            res.status(500).send({ msg: "Error de servidor!" });
+        } else {
+            res.status(200).send({
+                name: product.name,
+                description: product.description,
+                category: product.category,
+                quantity: product.quantity,
+                price: product.price,
+                image: product.image,
+            });
+        }
+    });
+}
 
 export const ProductsController = {
     createProduct,
     updateProduct,
     getAllProducts,
     getProductById,
-}
+};
--- src/routes/product.js
+++ src/routes/product.js
@@ -4,12 +4,14 @@ import multiparty from "connect-multiparty";
 import { mdAuth } from "../middlewares/index.js";
 import { ProductsController } from "../controllers/index.js";
 
+const mdUpload = multiparty({ uploadDir: "./uploads/images" });
+
 const api = express.Router();
 
 api.post("/product", [mdAuth.asureAuth], ProductsController.createProduct);
 api.get("/products", [mdAuth.asureAuth], ProductsController.getAllProducts);
 
-api.patch("/product/update/:id", [mdAuth.asureAuth], ProductsController.updateProduct);
 api.get("/product/getbyid/:id", [mdAuth.asureAuth], ProductsController.getProductById);
+api.patch("/product/update/:id", [mdAuth.asureAuth, mdUpload], ProductsController.updateProduct);
 
 export const productRoutes = api;
